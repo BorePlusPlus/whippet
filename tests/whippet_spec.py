ad068f5a98098c7b0ff49aa01aae1d19af89beb1
Dalibor Novak <dalibor79@gmail.com>
2019-08-30 14:44:51 +0100

    Basic implementation
    Overwrites existing hooks

--- tests/whippet_spec.py
+++ tests/whippet_spec.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+
+from _pytest.capture import CaptureFixture
+
 from whippet import __version__, whippet
 
 
@@ -5,5 +9,40 @@ def it_exposes_version():
     assert __version__ == "0.1.0"
 
 
-def it_increments():
-    assert whippet.inc(1) == 2
+def it_installs_hooks(tmp_path: Path) -> None:
+    hooks_dir = tmp_path / ".git" / "hooks"
+    hooks_dir.mkdir(parents=True)
+
+    whippet.install_hooks(tmp_path)
+
+    for hook in whippet.hook_list:
+        hook_path = hooks_dir / hook
+        assert hook_path.exists()
+        assert hook_path.is_file()
+
+
+def it_creates_hooks_dir_when_needed(tmp_path: Path) -> None:
+    git_dir = tmp_path / ".git"
+    git_dir.mkdir(parents=True)
+    hooks_dir = git_dir / "hooks"
+    assert not hooks_dir.exists()
+
+    whippet.install_hooks(tmp_path)
+
+    for hook in whippet.hook_list:
+        hook_path = hooks_dir / hook
+        assert hook_path.exists()
+        assert hook_path.is_file()
+
+
+def it_skips_installation_when_no_git_dir(tmp_path: Path, capsys: CaptureFixture) -> None:
+    whippet.install_hooks(tmp_path)
+    captured = capsys.readouterr()
+    hooks_dir = tmp_path / ".git" / "hooks"
+
+    assert "skipping hooks installation" in captured.out
+    assert not hooks_dir.exists()
+
+
+def it_installs_hooks_in_git_dir_in_ancestor_dir():
+    pass
--- whippet/whippet.py
+++ whippet/whippet.py
@@ -1,2 +1,65 @@
-def inc(x):
-    return x + 1
+from pathlib import Path
+from typing import Optional
+
+from whippet import __version__
+
+hook_list = [
+    "applypatch-msg",
+    "pre-applypatch",
+    "post-applypatch",
+    "pre-commit",
+    "prepare-commit-msg",
+    "commit-msg",
+    "post-commit",
+    "pre-rebase",
+    "post-checkout",
+    "post-merge",
+    "pre-push",
+    "pre-receive",
+    "update",
+    "post-receive",
+    "post-update",
+    "push-to-checkout",
+    "pre-auto-gc",
+    "post-rewrite",
+    "sendemail-validate",
+]
+
+
+def resolve_git_dir(cwd: Path) -> Optional[Path]:
+    cwd = cwd.resolve()
+    while True:
+        git_dir = cwd / ".git"
+        if git_dir.exists() and git_dir.is_dir():
+            return git_dir
+        if cwd == cwd.parent:
+            return None
+        cwd = cwd.parent
+
+
+def get_hooks_dir(cwd: Path) -> Optional[Path]:
+    git_dir = resolve_git_dir(cwd)
+    if git_dir is None:
+        return None
+
+    hook_dir = git_dir / "hooks"
+    hook_dir.mkdir(mode=0o775, exist_ok=True)
+    return hook_dir
+
+
+def install_hooks(cwd: Path) -> None:
+    hooks_dir = get_hooks_dir(cwd)
+    if hooks_dir is None:
+        print("whippet - Can not find .git directory, skipping hooks installation.")
+        return
+
+    with open(Path(__file__).parent / "hook_template") as ht:
+        template = ht.read()
+
+    for hook_name in hook_list:
+        hook_file = hooks_dir / hook_name
+        # if hook_file.exists():
+        #     print(f"{hook_name} hook script already exists - ")
+        hook = template.format(version=__version__, hook=hook_name)
+        with open(hook_file, "w") as hf:
+            hf.write(hook)

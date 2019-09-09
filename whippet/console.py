7ef1629db115044fe03425facb3454c261ed1bda
Dalibor Novak <dalibor79@gmail.com>
2019-09-09 16:25:22 +0100

    Extract install prompt

--- whippet/console.py
+++ whippet/console.py
@@ -2,21 +2,27 @@ from whippet import whippet
 from pathlib import Path
 
 
+def install_prompt(git_dir: Path) -> bool:
+    while True:
+        answer = input(
+            f"whippet - Are you sure you want to install hooks in {git_dir}? [Y/n] "
+        ).lower()
+        if answer == "n":
+            return False
+        if answer in ["y", ""]:
+            return True
+
+
 def run():
     cwd = Path.cwd()
     git_dir = whippet.resolve_git_dir(cwd)
+
     if git_dir is None:
         print("whippet - Can not find .git directory, skipping hooks installation.")
         return
 
-    while True:
-        answer = input(
-            f"whippet - Are you sure you want to install hooks in {git_dir}? [Y/n]"
-        ).lower()
-        if answer == "n":
-            print("whippet - Aborted.")
-            return
-        if answer in ["y", ""]:
-            break
+    if not install_prompt(git_dir):
+        print("whippet - Aborted.")
+        return
 
     whippet.install_hooks(cwd)
